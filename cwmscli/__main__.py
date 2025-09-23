7a3a8a92c55c4cc2aa7b4f724b1cf921c85da64d
Charles Graham <charles.r.graham@usace.army.mil>
2025-09-23 16:55:30 -0500

    Combine USGS Scripts into Group (#40)
    Combines the various USGS scripts into a click group allowing for
    subcommands
    
    Base Command
    ```bash
    > cwms-cli
    Usage: cwms-cli [OPTIONS] COMMAND [ARGS]...
    
    Options:
      --help  Show this message and exit.
    
    Commands:
      blob            Manage CWMS Blobs (upload, download, delete, update, list)
      csv2cwms        Store CSV TimeSeries data to CWMS using a config file
      shefcritimport  Import SHEF crit file into timeseries group for SHEF...
      usgs            USGS utilities
    ```
    
    USGS Command
    ```bash
    > cwms-cli usgs
    Usage: cwms-cli usgs [OPTIONS] COMMAND [ARGS]...
    
      USGS utilities
    
    Options:
      --help  Show this message and exit.
    
    Commands:
      measurements             Store USGS measurements into CWMS database
      ratings                  Get USGS ratings and store into CWMS database
      ratings-ini-file-import  Store rating ini file information into...
      timeseries               Get USGS timeseries values and store into CWMS...
    ```
    
    Measurements Command
    ```bash
    > cwms-cli usgs measurements --help
    Usage: cwms-cli usgs measurements [OPTIONS]
    
      Store USGS measurements into CWMS database
    
    Options:
      -d, --days_back_modified TEXT   Days back from current time measurements
                                      have been modified in USGS database. Can be
                                      integer value
      -c, --days_back_collected TEXT  Days back from current time measurements
                                      have been collected. Can be integer value
      -o, --office TEXT               Office to grab data for  [required]
      -a, --api_root TEXT             Api Root for CDA. Can be user defined or
                                      placed in a env variable CDA_API_ROOT
                                      [required]
      -k, --api_key TEXT              api key for CDA. Can be user defined or
                                      place in env variable CDA_API_KEY. one of
                                      api_key or api_key_loc are required
      -kl, --api_key_loc TEXT         file storing Api Key. One of api_key or
                                      api_key_loc are required
      -b, --backfill TEXT             Backfill POR data, use list of USGS IDs
                                      (e.g. 05057200, 05051300) or the word
                                      'group' to attempt to backfill all sites in
                                      the OFFICE id's Data Acquisition->USGS
                                      Measurements group
      --help                          Show this message and exit.
    ```

--- cwmscli/__main__.py
+++ cwmscli/__main__.py
@@ -1,8 +1,8 @@
 import click
 
 from cwmscli.commands import commands_cwms
-from cwmscli.getusgs import commands_getusgs
 from cwmscli.load import __main__ as load
+from cwmscli.usgs import usgs_group
 
 
 @click.group()
@@ -10,10 +10,7 @@ def cli():
     pass
 
 
-cli.add_command(commands_getusgs.getusgs_timeseries)
-cli.add_command(commands_getusgs.getusgs_ratings)
-cli.add_command(commands_getusgs.ratingsinifileimport)
-cli.add_command(commands_getusgs.getusgs_measurements)
+cli.add_command(usgs_group, name="usgs")
 cli.add_command(commands_cwms.shefcritimport)
 cli.add_command(commands_cwms.csv2cwms_cmd)
 cli.add_command(commands_cwms.blob_group)
