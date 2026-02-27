ef508ba481a47cd7bcbd99d1d5379f02abf67bb2
Charles Graham <charles.r.graham@usace.army.mil>
2026-02-27 11:10:38 -0600

    Add colorization method to cwms-cli (#118)
    With this PR you will be able to easily colorize the CLI
    
    Resolves #10 
    
    ## Docs?
    @Enovotny 
    Should decide if we want both developer and enduser docs. 
    
    If developer docs do I put those in CONTRIBUTING with the below howto?
    
    If enduser, would you like to see a page in addition to what would
    present automatically from the site builder made by the args?
    
    i.e. something explaining the colors and when/why you might disable them
    (file based logs)
    
    
    ## PR Details
    
    I started with blob which will dynamically figure out if an object is
    JSON and format it (screenshot below)
    
    I did have to hook into the logger to try and catch the logs at a low
    level.
    
    This also made it so you do not have to pass the no-color optione
    everywhere.
    
    This introduces `log-file` and `log-level` arguments. 
    
    
    Where you would call it like this:
    ```bash
    cwms-cli --log-level=DEBUG blob list
    ```
    
    Right after cwms-cli. Keeping the blob/list arg list from looking like a
    book.
    
    
    I have it coloring the log level by default in 4 different colors. 
    
    But to explicitly color a log item with JSON you can do this:
    
    ```python
    import logging
    from cwmscli.utils.logging.formatters import format_df_for_log
    
    
    logging.info(
                format_df_for_log(
                    df, c=c, col_colors={0: "BLUE", 1: "GREEN"}, json_color=True
                )
            )
    ```
    
    You can also arbitrarily color various parts of text with
    `c("some text", "BLUE")`
    
    or
    
    `logging.info(c("hello", "GREEN") + " " + c("world", "BLUE"))`
    which would give
    <img width="470" height="28" alt="image"
    src="https://github.com/user-attachments/assets/1be946ff-083d-422a-a8be-0c824fb4670c"
    />
    
    
    
    <img width="1184" height="399" alt="image"
    src="https://github.com/user-attachments/assets/dea12de1-964a-472b-ba18-4bdee5ed3a8b"
    />
    
    ### Libraries
    
    I went with Colorama, seemed very popular!

--- cwmscli/__main__.py
+++ cwmscli/__main__.py
@@ -1,20 +1,44 @@
-from __future__ import annotations
-
 import logging
 import os
 import sys
+from typing import Optional
 
 import click
 
 from cwmscli.commands import commands_cwms
 from cwmscli.load import __main__ as load
 from cwmscli.usgs import usgs_group
+from cwmscli.utils.logging import LoggingConfig, setup_logging
 from cwmscli.utils.ssl_errors import is_cert_verify_error, ssl_help_text
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
-def cli():
-    pass
+@click.option(
+    "--log-file",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+    default=None,
+    help="Write logs to a file. If set, disables color completely.",
+)
+@click.option(
+    "--no-color",
+    is_flag=True,
+    default=False,
+    help="Disable colored output in the terminal.",
+)
+@click.option(
+    "--log-level",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
+    ),
+    default="INFO",
+)
+def cli(log_file: Optional[str], no_color: bool, log_level: str) -> None:
+    level = getattr(logging, log_level.upper(), logging.INFO)
+
+    # Disable colors if stdout isn't a TTY (piped/redirected)
+    tty = sys.stdout.isatty()
+    color = (not no_color) and tty
+    setup_logging(LoggingConfig(level=level, log_file=log_file, color=color))
 
 
 cli.add_command(usgs_group, name="usgs")
