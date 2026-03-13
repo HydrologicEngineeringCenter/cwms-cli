59280d05b04a6e5c306be2368be190403cb5b847
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2026-03-13 21:27:09 +0000

    Add the authlib dep requirement lib

--- cwmscli/requirements.py
+++ cwmscli/requirements.py
@@ -1,6 +1,5 @@
-# A file containing shared minimum version requirements for dependencies
-# used by the `@requires` decorator in `cwmscli.utils.deps`.
-
+# Shared minimum version requirements for optional dependencies used by
+# the `@requires` decorator in `cwmscli.utils.deps`.
 
 cwms = {
     "module": "cwms",
@@ -22,4 +21,12 @@ dataretrieval = {
     "version": "1.0.10",
     "desc": "Loading hydrologic data from USGS",
     "link": "https://github.com/DOI-USGS/dataretrieval-python",
+}
+
+authlib = {
+    "module": "authlib",
+    "package": "authlib",
+    "version": "1.6.0",
+    "desc": "OAuth 2.0 and OpenID Connect client support for PKCE login and token refresh",
+    "link": "https://docs.authlib.org/",
 }
