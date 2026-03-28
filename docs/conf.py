455a0f890edc61464562a5114471e006a1c82ea2
Charles Graham <charles.r.graham@usace.army.mil>
2026-03-27 22:58:04 -0700

    Avoid circular reference failures in doc site

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ linkcheck_ignore = [
     r"^http://localhost(:\d+)?/.*",
     r"^http://127\.0\.0\.1(:\d+)?/.*",
     r"^https://www\.gnu\.org/software/bash/manual/bash\.html(#.*)?$",
+    r"^https://cwms-cli\.readthedocs\.io/.*$",
     r"^https://cwms-data\.usace\.army\.mil/cwms-data/regexp/?$",
 ]
 # autodoc_mock_imports = ["cwms", "pandas", "requests"]
