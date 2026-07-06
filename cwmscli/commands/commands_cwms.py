20cc807ac8e214b434cf682dc7b2e7c081eccd47
Thomas Ressin <thomas.s.ressin@usace.army.mil>
2026-07-06 10:50:03 -0400

    Test new yaml

--- cwmscli/commands/commands_cwms.py
+++ cwmscli/commands/commands_cwms.py
@@ -492,13 +492,15 @@ def update_cli_cmd(target_version: Optional[str], pre: bool, yes: bool) -> None:
 @click.group(
     "blob",
     help="Manage CWMS Blobs (upload, download, delete, update, list)",
-    epilog=textwrap.dedent("""
+    epilog=textwrap.dedent(
+        """
     Example Usage:\n
     - Store a PDF/image as a CWMS blob with optional description\n
     - Download a blob by id to your local filesystem\n
     - Update a blob's name/description/mime-type\n
     - Bulk list blobs for an office  
-"""),
+"""
+    ),
 )
 def blob_group():
     pass
@@ -706,12 +708,14 @@ def list_cmd(**kwargs):
 @click.group(
     "clob",
     help="Manage CWMS Clobs (upload, download, delete, update, list)",
-    epilog=textwrap.dedent("""
+    epilog=textwrap.dedent(
+        """
     Example Usage:\n
     - Download a clob by id to your local filesystem\n
     - Update a clob's name/description/mime-type\n
     - Bulk list clobs for an office  
-"""),
+"""
+    ),
 )
 @requires(reqs.cwms)
 def clob_group():
