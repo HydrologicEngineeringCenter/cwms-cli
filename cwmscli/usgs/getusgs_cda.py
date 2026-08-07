d7b97575576ce908794473d0cc58976fbdb3ffeb
Thomas Ressin <thomas.s.ressin@usace.army.mil>
2026-08-07 13:45:25 -0400

    Removed commented code that was replaced.

--- cwmscli/usgs/getusgs_cda.py
+++ cwmscli/usgs/getusgs_cda.py
@@ -395,16 +395,6 @@ def CWMS_writeData(USGS_ts, USGS_data, USGS_data_method, days_back):
                             ).first()
                             values = values_new.reset_index()
 
-                        # # 15 minute conversion to allow storing smaller interval data (eg. 5 minute) as 15 minute data
-                        # if ts_id.split(".")[3] == "15Minutes":
-                        #     values_dt = values.copy()
-                        #     values_dt["date-time"] = pd.to_datetime(
-                        #         values_dt["date-time"]
-                        #     )
-                        #     values_dt.set_index("date-time", inplace=True)
-                        #     values15 = values_dt.resample("15min").first()
-                        #     values = values15.reset_index()
-
                         # write values to CWMS database
                         try:
                             data = cwms.timeseries_df_to_json(
